27db6facd5e58616aa47ba89e54f3c9c2b3e6385
AlyonaKileynikova <105370125+AlyonaKileynikova@users.noreply.github.com>
2022-09-23 20:31:36 +0300

    HW-JS-06
    Corrected task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const listCategory = document.querySelectorAll('.item');
+const listCategory = document.querySelectorAll(".item");
 console.log("Number of categories:", listCategory.length);
 
 listCategory.forEach(function (category) {
-    console.log('Category:', category.firstElementChild.textContent)
-    console.log('elements:', category.lastElementChild.childElementCount)
+  console.log("Category:", category.firstElementChild.textContent);
+  console.log("elements:", category.lastElementChild.childElementCount);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,36 +1,63 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const productContainer = document.querySelector('#ingredients');
-let arrayLi = [];
+const productContainer = document.querySelector("#ingredients");
+const itemsLi = ingredients.map((item) => {
+  const itemsEl = document.createElement("li");
+  itemsEl.classList.add("item");
+  itemsEl.textContent = item;
+  return itemsEl;
+});
 
-for (let i = 0; i < ingredients.length; i = + 1) {
-  const itemsLi = document.createElement("li");
-  itemsLi.textContent = ingredients[i];
-  itemsLi.classList.add("item");
-  arrayLi.push(itemsLi);
-}
-  
-productContainer.append(...arrayLi);
+console.log(itemsLi);
+productContainer.append(...itemsLi);
 
+// const productContainer = document.querySelector("#ingredients");
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const itemsLi = document.createElement("li");
+//   itemsLi.textContent = ingredients[i];
+//   itemsLi.classList.add("item");
+//   productContainer.append(itemsLi);
+// }
+// console.log(productContainer);
 
+// let arrayLi = [];
+
+// arrayLi.forEach(function (ingredients) {
+//   const itemsLi = document.createElement('li');
+//   itemsLi.textContent = ingredients[i];
+//   itemsLi.classList.add('item');
+//   productContainer.append(itemsLi);
+//   console.log(productContainer);
+// })
+
+// productContainer.append(...arrayLi);
 
 // for (let i = 0; i < ingredients.length; i += 1) {
 //   const itemsLi = document.createElement('li');
-//   itemsLi.innerHTML = ingredients[i];
+//   itemsLi.textContent = ingredients[i];
 //   itemsLi.classList.add('item');
 //   productContainer.append(itemsLi);
 //   console.log(productContainer);
 // }
 
-// const elements = ingredients.map(...itemsLi);
+// const elements = ingredients.map(itemsLi);
 // console.log(elements);
 
 // productContainer.append(...elements);
 // console.log(productContainer);
+
+// for (let i = 0; i < ingredients.length; i = + 1) {
+//   const itemsLi = document.createElement("li");
+//   itemsLi.textContent = ingredients[i];
+//   itemsLi.classList.add("item");
+//   arrayLi.push(itemsLi);
+// }
+
+// productContainer.append(...arrayLi);
